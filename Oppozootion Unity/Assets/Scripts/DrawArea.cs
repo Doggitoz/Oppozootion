d6b3f4cb0ead050d3d2d7950cfaf24d4d6818c36
Ben Jenkins <73377556+CaudexEst@users.noreply.github.com>
2022-04-18 16:57:27 -0500

    drawing cards to draw area

---
+++ Oppozootion Unity/Assets/Scripts/DrawArea.cs
@@ -0,0 +1,73 @@
+/**** 
+ * Created by: Ben Jenkins
+ * Date Created: April 18,2022
+ * 
+ * Last Edited by: NA
+ * Last Edited: NA
+ * 
+ * Description: Handles generating new cards to the draw area
+****/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawArea : MonoBehaviour
+{
+    //Variables
+    public int numberOfAnimalCards=0;
+    public GameObject AnimalcardPrefab;
+    public GameObject[] AnimalcardSlots;
+    
+    
+    //Draw area exists between the points (-5,-5) and (5,5) with (0,0) being the center
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        AnimalcardSlots = new GameObject[numberOfAnimalCards];
+        GenerateAnimalCardSlots();
+    }
+
+    private void GenerateAnimalCardSlots()
+    {
+        float cardSpacing = 10f/((numberOfAnimalCards/2)+1);
+        float currentspacing = 0f;
+
+
+        for(int i = 0; i < numberOfAnimalCards; i++)
+        {
+            GameObject newSlot = new GameObject();
+            if (i % 2==0)
+            {
+                currentspacing += cardSpacing;
+            }
+            newSlot.transform.position = new Vector3((-5f)+currentspacing, 0, ((i % 2) * 2) - 1);
+            AnimalcardSlots[i] = newSlot;
+        }
+        Invoke("fillCards", 1f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void fillCards()
+    {
+        for(int i = 0; i < numberOfAnimalCards; i++)
+        {
+            if(/*determine how cards are taken from a slot*/true)
+            {
+                DrawCard(AnimalcardSlots[i].transform.position);
+            }
+        }
+    }
+
+    private void DrawCard(Vector3 pos)
+    {
+        GameObject Animalcard = Instantiate(AnimalcardPrefab);
+        Animalcard.transform.position = pos;
+    }
+}
