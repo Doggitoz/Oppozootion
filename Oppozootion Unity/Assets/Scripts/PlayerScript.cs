679a7d7712461ff49b8a41ecb5cf36b3bfd9dc7b
Doggitoz <cpwheeler4@gmail.com>
2022-04-25 16:43:47 -0500

    Saved

--- Oppozootion Unity/Assets/Scripts/PlayerScript.cs
+++ Oppozootion Unity/Assets/Scripts/PlayerScript.cs
@@ -19,29 +19,46 @@ public class PlayerScript : MonoBehaviour
 
     [HideInInspector] public float height = 2.5f;
     [HideInInspector] public float AnimalAreaWidth = 7.5f;
-    [HideInInspector] public float BundleAreaWidth = 2.5f;
+    [HideInInspector] public float BundleAreaHeight = 5f;
+
+    [HideInInspector] public GameObject thisGO;
+    public int cardsTakenByPlayer = 0;
+
+    private GameManager GM;
 
 
     //PlayerCardArea is 5 long and 2.5 wide for internal coords of (5,2.5) to (-5,-2.5)
     //AnimalCardArea is 7.5 long and 2.5 wide for internal coords of (5,2.5) to (-2.5,-2.5)
     //BundleCardArea is 2.5 long and 2.5 wide for internal coords of (-2.5,2.5) to (-5,-2.5)
 
+    private void Awake()
+    {
+        GM = GameManager.GM;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
         AnimalCards = new List<GameObject>();
-        BundleCards = new List<GameObject>(); 
+        BundleCards = new List<GameObject>();
+        thisGO = this.gameObject;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (cardsTakenByPlayer == 2)
+        {
+            cardsTakenByPlayer = 0;
+            GM.NextTurn();
+        }
         UpdateAnimalCards();
         UpdateBundleCards();
     }
 
     public void addAnimalCard(GameObject obj)
     {
+        obj.transform.parent = GM.player.transform;
         AnimalCards.Add(obj);
     }
     public void addBundleCard(GameObject obj)
@@ -51,12 +68,20 @@ public class PlayerScript : MonoBehaviour
 
     private void UpdateAnimalCards()
     {
+        float cardSpacing = AnimalAreaWidth / ((AnimalCards.Count / 2) + 1);
+        if (AnimalCards.Count % 2 == 1)
+        {
+            cardSpacing = AnimalAreaWidth / (((AnimalCards.Count + 1) / 2) + 1);
+        }
+        else
+        {
+            cardSpacing = AnimalAreaWidth / ((AnimalCards.Count / 2) + 1);
+        }
 
-<<<<<<< Updated upstream
-=======
+
         float currentspacing = 0f;
         //float AnimalCardHeightSpacing;
-        
+
 
         for (int i = 0; i < AnimalCards.Count; i++)
         {
@@ -64,13 +89,15 @@ public class PlayerScript : MonoBehaviour
             {
                 currentspacing += cardSpacing;
             }
-            if (AnimalCards[i] != null)
-                AnimalCards[i].transform.localPosition = new Vector3((5f) - currentspacing, 0, ((i % 2)*2.5f-1.25f));
+            AnimalCards[i].transform.localPosition = new Vector3((5f) - currentspacing, 0, ((i % 2) * 2.5f - 1.25f));
         }
->>>>>>> Stashed changes
     }
     private void UpdateBundleCards()
     {
-
+        float cardSpacing = BundleAreaHeight / ((BundleCards.Count / 2) + 1);
+        for (int i = 0; i < BundleCards.Count; i++)
+        {
+            BundleCards[i].transform.localPosition = new Vector3((-2.5f) - (2.5f / 2f), 0, 2.5f + cardSpacing); ;
+        }
     }
 }
